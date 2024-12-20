96f93009e96a5aff0b3c38c8a1a7bec1545f1d71
Omkar <omkarsalunke1978@gmail.com>
2024-12-20 16:50:32 +0530

    content scraped successfully.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_webpage
+from scrape import scrape_webpage, split_dom_content, clean_body_content, extract_body_content
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter URL")
@@ -8,3 +8,18 @@ if st.button("Scrape"):
     st.write("Scraping...")
     result = scrape_webpage(url)
     print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+    
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("What do you want to parse?")
+    if st.button("Parse"):
+        if parse_description:
+            st.write("Parsing...")
+            dom_chunks = split_dom_content(st.session_state.dom_content)
--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
 import time
+from bs4 import BeautifulSoup
 
 def scrape_webpage(website):
     print("Launching Chrome...")
@@ -9,11 +10,32 @@ def scrape_webpage(website):
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
-    try:
-        driver.get(website)
-        print("page loaded")
-        html = driver.page_source
-        time.sleep(10)
-        return html
-    finally:
-        driver.quit()
+    driver.get(website)
+    print("page loaded")
+    html = driver.page_source
+    time.sleep(10)
+    return html
+
+def extract_body_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    else:
+        return None
+    
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, 'html.parser')
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_body_content = soup.get_text(separator="\n")
+    clean_body_content = "\n".join(line.strip() for line in cleaned_body_content.splitlines() if line.strip())
+    return cleaned_body_content
+
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i : i + max_length]
+        for i in range(0, len(dom_content), max_length)
+    ]
